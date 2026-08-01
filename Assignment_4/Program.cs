af03e0d6c4d18fe8844d822cba3dd2f40c6edffe
Mariam Ashraf <mariamashraffffff999@gmail.com>
2026-08-02 01:26:45 +0300

    Part01 - Question_1

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -4,6 +4,23 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Part01
+
+        #region Question_1
+        /*a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?*/
+
+        //Answer -> DeliveryAddress is struct (value type),when it is copied, a new independent copy is created.
+        //so it doesn`t modify original variable.
+
+
+        /*b) What happens when a Customer variable is copied into another variable and one variable modifies the object? */
+
+        //Answer ->Customer is class (reference type),when it is copied, only the reference is copied.
+        //so it affects the original object and both variables refer to the same object.
+
+        #endregion
+
+        #endregion
+
     }
 }
